3b30243a0970ead189f75036128cdc60e4717589
Marcos khabie <marcos.khabie@ing.austral.edu.ar>
2017-05-23 22:36:28 -0300

    material

--- Material.c
+++ Material.c
@@ -7,20 +7,9 @@
 
 
 
-typedef struct material{
+Material* createMaterial(int typeOfMaterial,int  code, char* autor,char* title, char* editorial){
 
-    int numberOfBill;
-    // Flata un supuesto "NitHotel"
-    char* nameOfHotel;
-    char* nameOfClient;
-    char* idClient;
-    double amountToPay;
-}Material;
+}
 
-Bill* createBill(int numberOfBill,char* nameOfHotel, char* nameOfClient,char* idClient, double amountToPay);
-
-void payRoom(Bill* aBill, Receptionist* receptionist);
-
-void freeBill(Bill* bill);
 
 
--- Material.h
+++ Material.h
@@ -5,4 +5,40 @@
 #ifndef EJERCICIO2_MATERIAL_H
 #define EJERCICIO2_MATERIAL_H
 
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct material{
+
+    int typeOfMaterial; // 0 magazine, 1 book.
+    int code;
+    char* author;
+    char* title;
+    int year;
+    char* editorial;
+
+}Material;
+
+Material* createMaterial(int typeOfMaterial,int  code, char* author,char* title, char* editorial){
+    Material* newMaterial = malloc(sizeof(Material));
+    newMaterial->typeOfMaterial= typeOfMaterial;
+    newMaterial->code=code;
+
+    newMaterial->author= malloc(sizeof(char)* strlen(author)+1);
+    newMaterial->title= malloc(sizeof(char)* strlen(title)+1);
+    newMaterial->editorial= malloc(sizeof(char)* strlen(editorial)+1);
+
+    strcpy(newMaterial->author,author);
+    strcpy(newMaterial->title,title);
+    strcpy(newMaterial->editorial,editorial);
+
+    return newMaterial;
+}
+
+void destroyMaterial(Material* material){
+    free(material->author);
+    free(material->title);
+    free(material->editorial);
+    free(material);
+}
 #endif //EJERCICIO2_MATERIAL_H
