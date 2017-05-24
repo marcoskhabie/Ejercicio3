2138039239c82fb4dcc910781105a5d666a99271
Agustin Bettati <bettatiagustin@gmail.com>
2017-05-23 23:04:38 -0300

    Small commit

--- Library.h
+++ Library.h
@@ -5,4 +5,26 @@
 #ifndef EJERCICIO2_LIBRARY_H
 #define EJERCICIO2_LIBRARY_H
 
+#include "Material.h"
+
+typedef struct library{
+    Material** materials;
+    int amountOfMaterials;
+    int maxCapacityOfMaterials;
+
+    Person** clients;
+    int amountOfClients;
+    int maxCapacityOfClients;
+}Library;
+
+Library* createLibrary(int initialCapacity);
+void destroyLibrary(Library* library);
+
+void addNewMaterial(Library* library, Material* material);
+void addNewPerson(Library* library, Person* person);
+
+void removeMaterial(Library* library, int idOfMaterial);
+
+void makeLoanOfMaterialToPerson(Library* library, int idMaterial, int idPerson);
+
 #endif //EJERCICIO2_LIBRARY_H
--- Loan.h
+++ Loan.h
@@ -2,8 +2,8 @@
 // Created by Marcos khabie on 18/5/17.
 //
 
-#ifndef EJERCICIO2_BORROWING_H
-#define EJERCICIO2_BORROWING_H
+#ifndef EJERCICIO2_LOAN_H
+#define EJERCICIO2_LOAN_H
 
 #include "Material.h"
 
