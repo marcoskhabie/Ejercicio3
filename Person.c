c7502827393cf79cce6f3ef0951937bad8810cd9
Marcos khabie <marcos.khabie@ing.austral.edu.ar>
2017-05-23 23:28:45 -0300

    Person

--- Person.c
+++ Person.c
@@ -3,3 +3,12 @@
 //
 
 #include "Person.h"
+
+
+  Person* createPerson(int typeOfPerson, char* name, char* surname, int amountOfLoans, double debt, Loan** loans,int maxCapacityOfLoans );{
+
+}
+void makeLoan(Material* material, Person* person);
+
+void returnLoan(Person* person, int idLoan);
+void destroyPerson(Person* person);
--- Person.h
+++ Person.h
@@ -6,5 +6,23 @@
 #define EJERCICIO2_PERSON_H
 
 
+#include "Loan.h"
 
+typedef struct person{  
+
+
+    int typeOfPerson; // 0 Student, 1 Teacher. 
+    char* name; 
+    char* surname; 
+    Loan** loans;
+    int amountOfLoans; 
+    int maxCapacityOfLoans;
+    double debt;   }Person;
+
+
+      Person* createPerson(int typeOfPerson, char* name, char* surname, int amountOfLoans, double debt, Loan** loans,int maxCapacityOfLoans ); 
+    void makeLoan(Material* material, Person* person);
+
+    void returnLoan(Person* person, int idLoan);
+    void destroyPerson(Person* person);
 #endif //EJERCICIO2_PERSON_H
