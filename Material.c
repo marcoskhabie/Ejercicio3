80b5811be504bcc095d625090063b7a4ed0f396e
Agustin Bettati <bettatiagustin@gmail.com>
2017-05-23 23:09:35 -0300

    Changed material

--- Material.c
+++ Material.c
@@ -3,13 +3,31 @@
 //
 
 #include "Material.h"
+Material* createMaterial(int typeOfMaterial,int  code, char* author,char* title, char* editorial){
+    Material* newMaterial = malloc(sizeof(Material));
+    newMaterial->typeOfMaterial= typeOfMaterial;
+    newMaterial->code=code;
 
+    newMaterial->author= malloc(sizeof(char)* strlen(author)+1);
+    newMaterial->title= malloc(sizeof(char)* strlen(title)+1);
+    newMaterial->editorial= malloc(sizeof(char)* strlen(editorial)+1);
 
+    strcpy(newMaterial->author,author);
+    strcpy(newMaterial->title,title);
+    strcpy(newMaterial->editorial,editorial);
 
-
-Material* createMaterial(int typeOfMaterial,int  code, char* autor,char* title, char* editorial){
-
+    return newMaterial;
 }
+
+void destroyMaterial(Material* material){
+    free(material->author);
+    free(material->title);
+    free(material->editorial);
+    free(material);
+}
+
+
+
 
 
 
